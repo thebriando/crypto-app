5f8eeea702014971e24bdf99c9ab726ae7bffd51
thebriando <thebriando@gmail.com>
2020-01-15 09:32:31 -0800

    Updates

---
+++ precache-manifest.786cd9974ea0d3bb7c471622d52dbe16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12877cfdc5a52cbe927ae82343fee374",
+    "url": "/crypto-app/index.html"
+  },
+  {
+    "revision": "9a77b155a4c47f51bb0d",
+    "url": "/crypto-app/static/css/main.23c22d1d.chunk.css"
+  },
+  {
+    "revision": "6354c48ba41cec50f5e1",
+    "url": "/crypto-app/static/js/2.5b7dbdf0.chunk.js"
+  },
+  {
+    "revision": "549b90ec48101170bd480cdc3bbf8e6c",
+    "url": "/crypto-app/static/js/2.5b7dbdf0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a77b155a4c47f51bb0d",
+    "url": "/crypto-app/static/js/main.64474144.chunk.js"
+  },
+  {
+    "revision": "d797d0cc1a9bf6308f9c",
+    "url": "/crypto-app/static/js/runtime-main.541bfdd4.js"
+  }
+]);
